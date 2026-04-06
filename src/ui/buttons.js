dba4380ff838ec60fc11b811c7ec7ee55b55bb73
Lolilycho <85595489+Lolilycho@users.noreply.github.com>
2026-04-07 00:45:21 +0900

    Update buttons.js

--- src/ui/buttons.js
+++ src/ui/buttons.js
@@ -2,8 +2,29 @@ import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 
 export function gmButtons() {
   return new ActionRowBuilder().addComponents(
-    new ButtonBuilder().setCustomId("start_day").setLabel("昼開始").setStyle(ButtonStyle.Success),
-    new ButtonBuilder().setCustomId("start_night").setLabel("夜開始").setStyle(ButtonStyle.Primary),
-    new ButtonBuilder().setCustomId("end_game").setLabel("試合終了").setStyle(ButtonStyle.Danger)
+    new ButtonBuilder()
+      .setCustomId("create_game")
+      .setLabel("ゲーム開始")
+      .setStyle(ButtonStyle.Success),
+
+    new ButtonBuilder()
+      .setCustomId("start_day")
+      .setLabel("昼開始")
+      .setStyle(ButtonStyle.Primary),
+
+    new ButtonBuilder()
+      .setCustomId("start_night")
+      .setLabel("夜開始")
+      .setStyle(ButtonStyle.Secondary),
+
+    new ButtonBuilder()
+      .setCustomId("end_night")
+      .setLabel("夜終了")
+      .setStyle(ButtonStyle.Primary),
+
+    new ButtonBuilder()
+      .setCustomId("end_game")
+      .setLabel("試合終了")
+      .setStyle(ButtonStyle.Danger)
   );
 }
