af7899f4c3272a46a57546ac393455ef22961dd8
Lolilycho <85595489+Lolilycho@users.noreply.github.com>
2026-04-06 22:25:53 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import { Client, GatewayIntentBits } from "discord.js";
 import { handleMessage, handleInteraction } from "./game/manager.js";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-client.once("ready", () => console.log("Bot起動"));
+client.once("clientReady", () => {
+  console.log("Bot起動");
+});
 
 client.on("messageCreate", handleMessage);
 client.on("interactionCreate", handleInteraction);
