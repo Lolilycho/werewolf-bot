ea400ac81a3da38bff84813ea7d48386ea76dd88
Lolilycho <85595489+Lolilycho@users.noreply.github.com>
2026-04-07 00:36:31 +0900

    Update channels.js

--- src/game/channels.js
+++ src/game/channels.js
@@ -1,19 +1,33 @@
 export async function createGameChannels(guild) {
 
+  // カテゴリ作成
   const category = await guild.channels.create({
     name: "人狼",
     type: 4
   });
 
+  // ルール
+  const rule = await guild.channels.create({
+    name: "rule",
+    parent: category.id
+  });
+
+  // 投票
   const vote = await guild.channels.create({
     name: "投票",
     parent: category.id
   });
 
+  // GM
   const gm = await guild.channels.create({
     name: "gm",
     parent: category.id
   });
 
-  return { category, vote, gm };
+  return {
+    category,
+    rule,
+    vote,
+    gm
+  };
 }
